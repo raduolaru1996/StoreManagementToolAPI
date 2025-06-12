e9f76b950736949af9c142572121795d4172fe8a
Radu Olaru <raduolaru1996@gmail.com>
2025-06-12 20:30:48 +0300

    Initial commit: Set up Spring Boot project with basic structure and dependencies

---
+++ src/main/java/ro/raduolaru/storemanagementtool/StoreManagementTool/StoreManagementToolApplication.java
@@ -0,0 +1,13 @@
+package ro.raduolaru.storemanagementtool.StoreManagementTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManagementToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreManagementToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/raduolaru/storemanagementtool/StoreManagementTool/StoreManagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.raduolaru.storemanagementtool.StoreManagementTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagementToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
