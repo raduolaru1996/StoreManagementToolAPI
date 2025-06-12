184de54d5c11cf6e2f9be86212a65fa43202d096
Radu Olaru <raduolaru1996@gmail.com>
2025-06-12 23:55:11 +0300

    Add missing Transactional annotations

--- src/main/java/ro/raduolaru/storemanagementtool/StoreManagementTool/service/ProductService.java
+++ src/main/java/ro/raduolaru/storemanagementtool/StoreManagementTool/service/ProductService.java
@@ -3,6 +3,8 @@ package ro.raduolaru.storemanagementtool.StoreManagementTool.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import ro.raduolaru.storemanagementtool.StoreManagementTool.dto.ProductDto;
 import ro.raduolaru.storemanagementtool.StoreManagementTool.mapper.ProductMapper;
@@ -18,10 +20,12 @@ public class ProductService {
         this.productMapper = productMapper;
     }
 
+    @Transactional(propagation = Propagation.REQUIRED)
     public ProductDto addProduct(ProductDto productDto){
         return productMapper.toDto(productRepository.save(productMapper.toEntity(productDto)));
     }
 
+    @Transactional(readOnly = true)
     public List<ProductDto> getProducts(){
         return productRepository.findAll().stream()
             .map(productMapper::toDto)
