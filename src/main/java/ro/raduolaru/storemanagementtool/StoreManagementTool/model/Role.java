f665ccc321b2b44d48289594a6a42ed680ef7ea2
Radu Olaru <raduolaru1996@gmail.com>
2025-06-13 15:12:07 +0300

    Enable Spring security and add User, UserRepository and Role enum

---
+++ src/main/java/ro/raduolaru/storemanagementtool/StoreManagementTool/model/Role.java
@@ -0,0 +1,6 @@
+package ro.raduolaru.storemanagementtool.StoreManagementTool.model;
+
+public enum Role {
+    USER,
+    ADMIN,
+}
---
+++ src/main/java/ro/raduolaru/storemanagementtool/StoreManagementTool/model/User.java
@@ -0,0 +1,35 @@
+package ro.raduolaru.storemanagementtool.StoreManagementTool.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
